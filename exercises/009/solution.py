9ce600a6803b4bc3bf9af4261d6a97a53b73bb24
Elsa Abs <elsa.abs@cri-paris.org>
2015-09-21 16:55:08 +0200

    exercise 9

--- exercises/009/solution.py
+++ exercises/009/solution.py
@@ -14,4 +14,4 @@ phantom_menace = """Turmoil has engulfed the Galactic Republic. The\
  Chancellor has secretly dispatched two Jedi Knights, the guardians of\
  peace and justice in the galaxy, to settle the conflict"""
  
- len(phantom_menace)
+     len(phantom_menace)
