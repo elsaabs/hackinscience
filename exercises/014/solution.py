0e86a376574c10cff4b2a385b7444f6ca670077c
Elsa Abs <elsa.abs@cri-paris.org>
2015-09-21 19:28:28 +0200

    exercise 14

---
+++ exercises/014/solution.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 21 19:18:17 2015
+
+@author: elsa
+"""
+
+import sys
+if len(sys.argv) > 1:
+    print(sys.argv[1])
+else:
+    print('usage: python3 solution.py PARAM')
