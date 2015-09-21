2d156ff8cba433ef1c63d631dacd5017ccbac85b
Elsa Abs <elsa.abs@cri-paris.org>
2015-09-21 16:52:38 +0200

    exercise 9

---
+++ exercises/009/solution.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 21 16:49:52 2015
+
+@author: elsa
+"""
+
+phantom_menace = """Turmoil has engulfed the Galactic Republic. The\
+ taxation of trade routes to outlying star systems is in\
+ dispute. Hoping to resolve the matter with a blockade of deadly\
+ battleships, the greedy Trade Federation has stopped all shipping to\
+ the small planet of Naboo. While the congress of the Republic\
+ endlessly debates this alarming chain of events, the Supreme\
+ Chancellor has secretly dispatched two Jedi Knights, the guardians of\
+ peace and justice in the galaxy, to settle the conflict"""
+ len(phantom_menace)
+ 
