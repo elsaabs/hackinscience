7ab5b49192c47ade4ba57a49eec88b2620e11def
Elsa Abs <elsa.abs@cri-paris.org>
2015-09-22 19:25:13 +0200

    exercise 102

--- exercises/102/solution.py
+++ exercises/102/solution.py
@@ -43,7 +43,7 @@ def check_my_city(cityvar):
     zip = []
     for d in velib:
         if d['city'].lower().rstrip('-') == cityvar:
-            zip.append(velib['zip'])
+            zip.append(velib[d]['zip'])
             return {'stations_nb': len(zip), 'zip code': zip, 'city': cityvar}
         else:
             print("Sorry! No station for your city has been found!")
