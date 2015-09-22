de9f2a269b0c52a60098b9b057f2065216d377ff
Elsa Abs <elsa.abs@cri-paris.org>
2015-09-22 16:15:15 +0200

    exercise 97

--- exercises/097/solution.py
+++ exercises/097/solution.py
@@ -7,8 +7,8 @@ Created on Tue Sep 22 15:37:07 2015
 
 
 def love_meet(l1, l2):
-    set(l1).intersection(l2)
+    return set(l1).intersection(l2)
 
 
 def affair_meet(l1, l2, l3):
-    set(l2).intersection(l3) - set(l1).intersection(l2)
+    return set(l2).intersection(l3) - set(l1).intersection(l2)
