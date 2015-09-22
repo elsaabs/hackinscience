5f0f6e1bc31fe14fe4366387e266173ec65832e1
Elsa Abs <elsa.abs@cri-paris.org>
2015-09-22 12:09:14 +0200

    exercise 50

--- exercises/050/solution.py
+++ exercises/050/solution.py
@@ -6,7 +6,7 @@ Created on Tue Sep 22 11:50:19 2015
 """
 
 multiple = []
-for i in range(1, 1000):
+for i in range(1, 1000//3+1):
     if 3*i < 1000:
         multiple.append(3*i)
     if 5*i < 1000:
