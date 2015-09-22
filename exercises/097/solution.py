15c19fe10d86cd34b33b694c46806b547fb65369
Elsa Abs <elsa.abs@cri-paris.org>
2015-09-22 16:10:26 +0200

    exercise 97

---
+++ exercises/097/solution.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 15:37:07 2015
+
+@author: elsa
+"""
+
+
+def love_meet(l1, l2):
+    set(l1).intersection(l2)
+
+
+def affair_meet(l1, l2, l3):
+    set(l2).intersection(l3) - set(l1).intersection(l2)
