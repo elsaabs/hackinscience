2bcd15318f73e0cdedbf04175b644eed5b47bfca
Elsa Abs <elsa.abs@cri-paris.org>
2015-09-22 17:17:05 +0200

    exercise 100

--- exercises/100/solution.py
+++ exercises/100/solution.py
@@ -6,11 +6,11 @@ Created on Tue Sep 22 16:43:47 2015
 """
 
 station = {
- 'address': 'RUE DES CHAMPEAUX (PRES DE LA GARE ROUTIERE) - 93170 BAGNOLET',
- 'number': 31705,
- 'latitude': 48.8645278209514,
- 'name': 'CHAMPEAUX (BAGNOLET)',
- 'longitude': 2.416170724425901
+    'address': 'RUE DES CHAMPEAUX (PRES DE LA GARE ROUTIERE) - 93170 BAGNOLET',
+    'number': 31705,
+    'latitude': 48.8645278209514,
+    'name': 'CHAMPEAUX (BAGNOLET)',
+    'longitude': 2.416170724425901
 }
 print('latitude', station['latitude'])
 print('longitude', station['longitude'])
