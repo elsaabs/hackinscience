f04b1e593d32879de5d1383eaf36b714e7a8a398
Elsa Abs <elsa.abs@cri-paris.org>
2015-09-22 15:24:57 +0200

    exercise 90

---
+++ exercises/090/solution.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 15:05:09 2015
+
+@author: elsa
+"""
+
+import sys
+l = list(enumerate(sys.argv))
+for i in range(0, len(sys.argv)):
+    print(l[i])
