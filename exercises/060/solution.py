523a402fc4f8c54b4276e8b24ec7714ba8945d85
Elsa Abs <elsa.abs@cri-paris.org>
2015-09-22 12:51:52 +0200

    exercise 60

---
+++ exercises/060/solution.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 12:29:21 2015
+
+@author: elsa
+"""
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+for i in range(0, 26):
+    for j in range(0, 26):
+        print(alphabet[i] + alphabet[j])
