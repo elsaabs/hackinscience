6b36c194f5767c19261240ed922c2304ff982dfa
Elsa Abs <elsa.abs@cri-paris.org>
2015-09-23 13:55:26 +0200

    exercise 110

--- exercises/110/solution.py
+++ exercises/110/solution.py
@@ -6,16 +6,13 @@ Created on Wed Sep 23 10:26:15 2015
 """
 
 import sys
-
-if sys.argv[2] == "+":
-    print(int(sys.argv[1]) + int(sys.argv[3]))
-if sys.argv[2] == "-":
-    print(int(sys.argv[1]) - int(sys.argv[3]))
-if sys.argv[2] == "/":
-    print(int(sys.argv[1]) / int(sys.argv[3]))
-if sys.argv[2] == "\*":
-    print(int(sys.argv[1]) * int(sys.argv[3]))
-if sys.argv[2] == "^":
-    print(int(sys.argv[1]) ** int(sys.argv[3]))
-if sys.argv[2] == "%":
-    print(int(sys.argv[1]) % int(sys.argv[3]))
+import operator
+operators = {'+': operator.add, '-': operator.sub, '/': operator.truediv,
+             '\*': operator.mul, '^': operator.pow, '%': operator.mod}
+if len(sys.argv) == 4:
+    try:
+        print(operators[sys.argv[2]](int(sys.argv[1]), int(sys.argv[3])))
+    except (ValueError, ZeroDivisionError, KeyError):
+        print('input error')
+else:
+    print('usage: ./solution.py a_number (an_operator +-*/%^) a_number')
