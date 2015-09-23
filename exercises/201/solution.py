21b4156dd547f60d60c7eb471772160c80acf0e9
Elsa Abs <elsa.abs@cri-paris.org>
2015-09-23 16:41:01 +0200

    exercise 201

---
+++ exercises/201/solution.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 23 16:11:25 2015
+
+@author: elsa
+"""
+
+
+def is_alpha(input):
+    if (input.isalpha() or input.isspace()):
+        return True
+    return False
