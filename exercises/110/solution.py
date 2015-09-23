91d07f6180ee65da501be510f88e28ca4a2c0929
Elsa Abs <elsa.abs@cri-paris.org>
2015-09-23 13:56:31 +0200

    exercise 110

--- exercises/110/solution.py
+++ exercises/110/solution.py
@@ -8,7 +8,7 @@ Created on Wed Sep 23 10:26:15 2015
 import sys
 import operator
 operators = {'+': operator.add, '-': operator.sub, '/': operator.truediv,
-             '\*': operator.mul, '^': operator.pow, '%': operator.mod}
+             '*': operator.mul, '^': operator.pow, '%': operator.mod}
 if len(sys.argv) == 4:
     try:
         print(operators[sys.argv[2]](int(sys.argv[1]), int(sys.argv[3])))
