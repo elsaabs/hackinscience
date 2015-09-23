a5227262192cbdd086367fdfae0e5fbf947bf27a
Elsa Abs <elsa.abs@cri-paris.org>
2015-09-23 10:12:47 +0200

    exercise 107

---
+++ exercises/107/solution.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 23 09:37:35 2015
+
+@author: elsa
+"""
+
+
+def select_student(my_class, min):
+    accepted = []
+    refused = []
+    for student in my_class:
+        print(student)
+        if student[1] > min:
+            accepted.append(student)
+        else:
+            refused.append(student)
+    return {'Accepted': accepted, 'Refused': refused}
