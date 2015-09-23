a4be13c1d08a67f6f634d10f76a80d129a2d7678
Elsa Abs <elsa.abs@cri-paris.org>
2015-09-23 17:17:57 +0200

    exercise 202

---
+++ exercises/202/solution.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 23 16:57:13 2015
+
+@author: elsa
+"""
+
+
+def starts_with(input, value):
+    if input.startswith(value):
+        return True
+    return False
