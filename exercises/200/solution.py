42b379b33c059d8dec9657800db733d13e4ed8b4
Elsa Abs <elsa.abs@cri-paris.org>
2015-09-23 16:08:12 +0200

    exercise 200

--- exercises/200/solution.py
+++ exercises/200/solution.py
@@ -7,7 +7,10 @@ Created on Wed Sep 23 15:29:21 2015
 
 
 def is_prime(num):
-    for i in range(2, int(num**0.5)+1):
-        if num % i == 0:
-            return False
-    return True
+    if num == 1:
+        return False
+    else:
+        for i in range(2, int(num**0.5)+1):
+            if num % i == 0:
+                return False
+        return True
