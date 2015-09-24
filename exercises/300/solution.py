774291cb0db8e3d2c45c1fe10d692aa06aa75363
Elsa Abs <elsa.abs@cri-paris.org>
2015-09-24 21:01:15 +0200

    exercise 300

--- exercises/300/solution.py
+++ exercises/300/solution.py
@@ -5,7 +5,7 @@ Created on Thu Sep 24 19:56:55 2015
 @author: elsa
 """
 
-import requests
-url = "http://julien.palard.fr/x/words"
-result = requests.get(url)
-print(result.text)
+doc = open('words.txt')
+list = [line.split(',') for line in doc.readlines()]
+for i in range(0, len(list)):
+    print(''.join(list[i]).rstrip('\n'))
