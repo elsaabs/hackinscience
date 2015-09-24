39217b85e5471cf17313b2ae890cd52fd0799976
Elsa Abs <elsa.abs@cri-paris.org>
2015-09-24 09:44:57 +0200

    exercise 210

--- exercises/210/solution.py
+++ exercises/210/solution.py
@@ -8,6 +8,6 @@ Created on Thu Sep 24 09:17:24 2015
 import is_prime
 prime1000 = []
 for i in range(1, 1000):
-    if is_prime.is_prime(i) == True:
+    if is_prime.is_prime(i):
         prime1000.append(i)
 print(sum(prime1000))
