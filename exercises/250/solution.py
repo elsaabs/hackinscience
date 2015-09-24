e2881bdfe49a6e81f58ee779fa80b273d1bd5e64
Elsa Abs <elsa.abs@cri-paris.org>
2015-09-24 16:19:08 +0200

    exercise 250

---
+++ exercises/250/solution.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 24 15:31:22 2015
+
+@author: elsa
+"""
+
+
+def draw_n_squares(n):
+    for i in range(0, n):
+        print('+---'*n + '+')
+        print('|   '*n + '|')
+    print('+---'*n + '+')
+
+'''
+ return '+---+\n|   |\n+---+'
+ '''
