50e9ec67ee68b66bee556944b8413e81e23197d1
Elsa Abs <elsa.abs@cri-paris.org>
2015-09-24 15:25:02 +0200

    exercise 240

---
+++ exercises/240/solution.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 24 15:10:54 2015
+
+@author: elsa
+"""
+
+a, b = 0, 1
+fib = []
+while b < 60:
+    fib.append(b)
+    a, b = b, a+b
+print(', '.join(str(item) for item in fib))
+
+'''
+a, b = 0, 1
+fib = []
+for len(fib) in range(0, 10):
+    fib.append(b)
+    a, b = b, a+b
+print(fib)
+'''
