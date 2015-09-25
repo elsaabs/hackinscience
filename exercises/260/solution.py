1c8e6dd0256da9b0d6c5b3cae65cc2b365a8c7b4
Elsa Abs <elsa.abs@cri-paris.org>
2015-09-25 11:36:50 +0200

    exercise 260

---
+++ exercises/260/solution.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 24 17:12:17 2015
+
+@author: elsa
+"""
+import math
+import numpy as np
+
+
+def euclidean(a, b):
+    return ((b[0]-a[0]) ** 2 + (b[1]-a[1]) ** 2) ** 0.5
+
+
+def opt_euclidean(a, b):
+    x = b[0] - a[0]
+    y = b[1] - a[1]
+    return math.hypot(x, y)
+
+
+def np_euclidean(a, b):
+    return np.sqrt(np.sum(np.power(np.array(a) - np.array(b), 2)))
