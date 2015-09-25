64e2f2356502b5afa7a27149433905642c6212ca
Elsa Abs <elsa.abs@cri-paris.org>
2015-09-25 09:55:03 +0200

    exercise 300

--- exercises/300/solution.py
+++ exercises/300/solution.py
@@ -5,7 +5,7 @@ Created on Thu Sep 24 19:56:55 2015
 @author: elsa
 """
 
-doc = open('words.txt')
+doc = open('words.txt', encoding="utf-8")
 list = [line.split(',') for line in doc.readlines()]
 for i in range(0, len(list)):
     print(''.join(list[i]).rstrip('\n'))
